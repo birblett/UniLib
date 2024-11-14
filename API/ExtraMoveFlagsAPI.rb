574526cf8d3b7b36a9729898a0988cffc05a13c4
birblett <124317329+birblett@users.noreply.github.com>
2024-11-14 10:20:09 -0800

    add grappling move flag

---
+++ API/ExtraMoveFlagsAPI.rb
@@ -0,0 +1,44 @@
+# ======================================================================================================================================== #
+# ============================================================= DEPENDENCIES ============================================================= #
+# ======================================================================================================================================== #
+
+UniLib.verify_version(0.6, __FILE__)
+
+# ======================================================================================================================================== #
+# ============================================================== PUBLIC API ============================================================== #
+# ======================================================================================================================================== #
+
+class PokeBattle_Move
+
+  def is_bite_move?
+    PBStuff::BITEMOVE.include?(@move)
+  end
+
+  def is_dance_move?
+    PBStuff::DANCEMOVE.include?(@move)
+  end
+  def is_full_body_move?
+    UniLib::FULL_BODY_MOVES.include?(@move)
+  end
+
+  def is_grappling_move?
+    UniLib::GRAPPLING_MOVES.include?(@move)
+  end
+
+  def is_hand_move?
+    UniLib::HAND_MOVES.include?(@move)
+  end
+
+  def is_kicking_move?
+    UniLib::KICKING_MOVES.include?(@move)
+  end
+
+  def is_stabbing_move?
+    PBStuff::STABBINGMOVE.include?(@move)
+  end
+
+  def is_wind_move?
+    UniLib::WIND_MOVES.include?(@move)
+  end
+
+end
--- Core/ExtraMoveFlagsCore.rb
+++ Core/ExtraMoveFlagsCore.rb
@@ -4,35 +4,3 @@
 
 UniLib.verify_version(0.6, __FILE__)
 UniLib.include "Constants"
-
-# ======================================================================================================================================== #
-# ============================================================ INTERNAL/CORE ============================================================= #
-# ======================================================================================================================================== #
-
-class PokeBattle_Move
-
-  def is_bite_move?
-    PBStuff::BITEMOVE.include?(@move)
-  end
-
-  def is_full_body_move?
-    UniLib::FULL_BODY_MOVES.include?(@move)
-  end
-
-  def is_hand_move?
-    UniLib::HAND_MOVES.include?(@move)
-  end
-
-  def is_kicking_move?
-    UniLib::KICKING_MOVES.include?(@move)
-  end
-
-  def is_stabbing_move?
-    PBStuff::STABBINGMOVE.include?(@move)
-  end
-
-  def is_wind_move?
-    UniLib::WIND_MOVES.include?(@move)
-  end
-
-end
