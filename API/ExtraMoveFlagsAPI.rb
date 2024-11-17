16bcf861a801ae2df273b98c5456951146b9ad19
birblett <124317329+birblett@users.noreply.github.com>
2024-11-16 23:35:05 -0800

    add jaw lock to bite moves

--- API/ExtraMoveFlagsAPI.rb
+++ API/ExtraMoveFlagsAPI.rb
@@ -11,7 +11,7 @@ UniLib.verify_version(0.6, __FILE__)
 class PokeBattle_Move
 
   def is_bite_move?
-    PBStuff::BITEMOVE.include?(@move)
+    UniLib::BITEMOVE.include?(@move)
   end
 
   def is_dance_move?
