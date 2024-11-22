1f55a2dc871d27e191d837fdff5bd3e0856083c1
birblett <124317329+birblett@users.noreply.github.com>
2024-11-22 15:28:37 -0800

    support for adding static pokemon to maps

--- API/Map.rb
+++ API/Map.rb
@@ -10,6 +10,14 @@ UniLib.verify_version(0.6, __FILE__)
 
 module MapEvent
 
+  def self.set_debug(default=true)
+    $map_debug = default
+  end
+
+  def self.event_cmd(code, indent, args)
+    RPG::EventCommand.new(code, indent, args)
+  end
+
   def self.add_map_event(mapid, func=nil, &block)
     MAP_EVENTS[mapid] = [] unless MAP_EVENTS[mapid]
     if func.nil? and block.nil?
@@ -19,28 +27,80 @@ module MapEvent
     MAP_EVENTS[mapid].push(func.nil? ? block : func)
   end
 
-  def self.set_debug(default=true)
-    $map_debug = default
-  end
-
-  def self.add_item(map, x, y, name, item, switch, graphic = "Object ball")
+  def self.add_overworld_item(map, x, y, name, item, switch, graphic = "Object ball", **kwargs)
     i = map.events.size + 1
     event = RPG::Event.new(x, y)
-    event.instance_variable_set(:@id, i)
-    event.instance_variable_set(:@name, name)
+    event.id = i
+    event.name = name
     page = event.pages[0]
-    page.instance_variable_get(:@graphic).instance_variable_set(:@character_name, graphic)
-    page.instance_variable_set(:@move_speed, 1)
-    page.instance_variable_set(:@move_frequency, 1)
-    page.list.insert(0, RPG::EventCommand.new(111, 0, [12, "Kernel.pbItemBall(:#{item}); $unilib_switches[:#{switch}] = true"]))
-    page.list.insert(1, RPG::EventCommand.new(121, 1, [switch, switch, 0]))
-    page.list.insert(2, RPG::EventCommand.new(0, 1, []))
-    page.list.insert(3, RPG::EventCommand.new(412, 0, []))
+    page.graphic.character_name = graphic
+    kwargs[:graphic].each { |k, v| page.graphic.instance_variable_set("@#{k}".to_sym, v) } if kwargs[:graphic]
+    page.move_speed = 1
+    page.move_frequency = 1
+    page.list.insert(0, event_cmd(111, 0, [12, "Kernel.pbItemBall(:#{item}); $unilib_switches[:#{switch}] = true"]))
+    page.list.insert(1, event_cmd(121, 1, [switch, switch, 0]))
+    page.list.insert(2, event_cmd(0, 1, []))
+    page.list.insert(3, event_cmd(412, 0, []))
     page = RPG::Event::Page.new
-    page.condition.instance_variable_set(:@switch1_id, switch)
-    page.condition.instance_variable_set(:@switch1_valid, true)
+    page.condition.switch1_id = switch
+    page.condition.switch1_valid = true
     event.pages.push(page)
     map.events[i] = event
+  end
+
+  def self.add_static_pkmn(map, x, y, name, pkmn, lvl, asset, switch, **kwargs)
+    i = map.events.size + 1
+    event = static_pkmn(x, y, name, pkmn, lvl, asset, switch, **kwargs)
+    event.id = i
+    map.events[i] = event
+  end
+
+  def self.add_static_pkmn_obj(map, obj)
+    i = map.events.size + 1
+    event = obj.clone
+    event.id = i
+    map.events[i] = event
+  end
+
+  def self.static_pkmn(x, y, name, pkmn, lvl, asset, switch, **kwargs)
+    event = RPG::Event.new(x, y)
+    event.name = name
+    page = event.pages[0]
+    page.condition.switch1_id = switch
+    page.condition.switch1_valid = true
+    page = RPG::Event::Page.new
+    page.condition.switch1_id = switch
+    page.graphic.character_name = asset
+    page.graphic.direction = kwargs[:dir] ? kwargs[:dir] : 2
+    page.move_type = kwargs[:move_type] ? kwargs[:move_type] : 0
+    page.move_speed = kwargs[:move_speed] ? kwargs[:move_speed] : 3
+    page.move_frequency = kwargs[:move_freq] ? kwargs[:move_freq] : 3
+    page.move_route.list.insert(0, RPG::MoveCommand.new(25))
+    page.step_anime = true
+    page.direction_fix = kwargs[:fix_dir] ? true : false
+    events = []
+    events.push(event_cmd(250, 0, [RPG::AudioFile.new(kwargs[:sfx])])) if kwargs[:sfx] # sound effect
+    events.push(event_cmd(101, 0, [kwargs[:txt]])) if kwargs[:txt] # text
+    events.push(event_cmd(355, 0, [kwargs[:prescript]])) if kwargs[:prescript] # pre battle scripts
+    form, wild = kwargs[:form] ? kwargs[:form] : 0, kwargs[:wild]
+    events.push(event_cmd(122, 0, [545, 545, 0, 0, 102])) if wild
+    events.push(event_cmd(355, 0, ["m = pbGenerateWildPokemon(:#{pkmn}, #{lvl}); m.form=#{form}; pbWildBattleObject(m)"]))
+    events.push(event_cmd(122, 0, [545, 545, 0, 0, 0])) if wild
+    events.push(event_cmd(111, 0, [kwargs[:wincon] ? kwargs[:wincon] : 1, 100, 0, kwargs[:comparator] ? kwargs[:comparator] : 1, 0])) # if victory
+    if kwargs[:should_remove] # removes event and sets a permanent switch
+      events.push(event_cmd(116, 1, [1, 100, 0, 1, 0]))
+      events.push(event_cmd(355, 1, ["$unilib_switches[:#{switch}] = true"]))
+    end
+    events.push(event_cmd(355, 1, [kwargs[:winscript]])) if kwargs[:winscript] # run win script
+    events.push(event_cmd(101, 1, [kwargs[:wintxt]])) if kwargs[:wintxt] # display win text
+    events.push(event_cmd(0, 1, [])) # dummy
+    events.push(event_cmd(411, 0, [])) # else
+    events.push(event_cmd(115, 1, [])) # stop event
+    events.push(event_cmd(0, 1, [])) # dummy
+    events.push(event_cmd(412, 0, [])) # end conditional
+    page.list.prepend(*events)
+    event.pages.prepend(0, page)
+    event
   end
 
 end
