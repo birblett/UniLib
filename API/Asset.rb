74093843fa3c712cf49d109e5e831fc815298096
birblett <124317329+birblett@users.noreply.github.com>
2024-11-24 01:12:03 -0800

    add music asset redirection

--- API/Asset.rb
+++ API/Asset.rb
@@ -10,8 +10,27 @@ UniLib.verify_version(0.6, __FILE__)
 
 module Assets
 
-  def self.redirect(base, target)
-    ANIMATED_BITMAP_REDIRECT[base] = "Data/Mods/#{target}"
+  def self.redirect(type, base, target=nil, &block)
+    return if target.nil? and block.nil?
+    case type
+    when :BMP then ANIMATED_BITMAP_REDIRECT[base] = block ? ["Data/Mods/", block] : "Data/Mods/#{target}"
+    when :AUDIO then AUDIO_FILE_REDIRECT[base] = block ? ["../../Data/Mods/", block] : "../../Data/Mods/#{target}"
+    else print "Unsupported asset redirection type: #{type}"
+    end
+    self
+  end
+
+  def self.set_bmp_debug_log(default=true)
+    $unilib_bmp_asset_log = default
+  end
+
+  def self.set_audio_debug_log(default=true)
+    $unilib_audio_asset_log = default
+  end
+
+  def self.register_bgm_provider(type, bgms, persistent=false)
+    id = (persistent and BGM_REGISTRY[type]) ? BGM_REGISTRY[type][1] : -1
+    BGM_REGISTRY[type] = [bgms, id]
   end
 
 end
--- Core/AssetCore.rb
+++ Core/AssetCore.rb
@@ -11,12 +11,40 @@ UniLib.verify_version(0.6, __FILE__)
 module UniLib
 
   ANIMATED_BITMAP_REDIRECT = {}
+  AUDIO_FILE_REDIRECT = {}
 
 end
 
 module Assets
 
   include UniLib
+  $unilib_bmp_asset_log = false
+  $unilib_audio_asset_log = false
+  BGM_REGISTRY = {}
+
+  def self.get_asset(hash, str)
+    out = hash[str]
+    out = out[0] + (out[1].call) if out.is_a? Array
+    out.gsub!("../../", "") if str.start_with? "Audio"
+    out
+  end
+
+  def self.log(str)
+    str = str.name if str.is_a? RPG::AudioFile
+    UniLib.dev_log(str) unless %w[bump menu Choose menuclose].include? str
+  end
+
+  def self.strip_bgm(str)
+    str.gsub(/(Audio\/[^\/]*\/|\.\.\/\.\.\/|Data\/Mods\/)/, "")
+  end
+
+  def self.bgm_provider(type)
+    proc do
+      bgms = BGM_REGISTRY[type]
+      next $game_system.playing_bgm.name if $game_system.playing_bgm and bgms[0].include?(Assets.strip_bgm($game_system.playing_bgm.name))
+      bgms[1] = (bgms[0] - [BGM_REGISTRY[type][1]]).sample
+    end
+  end
 
 end
 
@@ -24,4 +52,48 @@ end
 # ================================================================ PATCH ================================================================= #
 # ======================================================================================================================================== #
 
-UniLib.insert_in_method(:AnimatedBitmap, :initialize, :HEAD, "file = UniLib::ANIMATED_BITMAP_REDIRECT[file] if UniLib::ANIMATED_BITMAP_REDIRECT[file]")
+module Audio
+
+
+  UNILIB_BGM_PLAY_OLD = singleton_method(:bgm_play) unless UniLib.lib_loaded(__FILE__)
+  define_singleton_method(:bgm_play) do |file, v=100, p=100|
+    Assets.log(file) if $unilib_audio_asset_log
+    play = true
+    if UniLib::AUDIO_FILE_REDIRECT[file]
+      file = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, file)
+      next if $game_system.playing_bgm and Assets.strip_bgm(file) == Assets.strip_bgm($game_system.playing_bgm.name) and $game_system.playing_bgm.volume == v and $game_system.playing_bgm.pitch == p
+    end
+    UNILIB_BGM_PLAY_OLD.(file, v, p) if play
+  end
+
+end
+
+UniLib.insert_in_method(:AnimatedBitmap, :initialize, :HEAD, "file = Assets.get_asset(UniLib::ANIMATED_BITMAP_REDIRECT, file) if UniLib::ANIMATED_BITMAP_REDIRECT[file]")
+
+UniLib.insert_in_function(:pbStringToAudioFile, :HEAD, "str = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, str) if UniLib::AUDIO_FILE_REDIRECT[str]")
+
+UniLib.insert_in_function(:pbResolveAudioFile, "if str.is_a?(String)", "str = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, str) if UniLib::AUDIO_FILE_REDIRECT[str]")
+
+UniLib.insert_in_method(:Scene_Map ,:autofade , "actual_bgm_name = $previous_map.bgm.name.clone", "actual_bgm_name = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, actual_bgm_name) if UniLib::AUDIO_FILE_REDIRECT[actual_bgm_name]; return if $game_system.playing_bgm and Assets.strip_bgm(actual_bgm_name) == Assets.strip_bgm($game_system.playing_bgm.name)")
+
+UniLib.insert_in_function(:pbBGMPlay, "return if !param ",
+  "s = param.is_a?(RPG::AudioFile) ? param.name : param
+  if UniLib::AUDIO_FILE_REDIRECT[s]
+    param = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, s)
+    return if $game_system.playing_bgm and Assets.strip_bgm(param) == Assets.strip_bgm($game_system.playing_bgm.name)
+  end")
+
+UniLib.insert_in_function(:pbCueBGM, "return if !bgm",
+  "s = bgm.is_a?(RPG::AudioFile) ? bgm.name : bgm
+  if UniLib::AUDIO_FILE_REDIRECT[s]
+    bgm = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, s)
+    return if $game_system.playing_bgm and Assets.strip_bgm(bgm) == Assets.strip_bgm($game_system.playing_bgm.name)
+  end")
+
+UniLib.insert_in_function(:pbGetTrainerBattleBGM, "if $PokemonGlobal.nextBattleBGM", "p = $PokemonGlobal.nextBattleBGM; s = p.is_a?(RPG::AudioFile) ? p.name : p;  p = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, s) if UniLib::AUDIO_FILE_REDIRECT[s]")
+
+UniLib.insert_in_function(:pbGetTrainerBattleBGM, "music=$cache.trainertypes[trainertype].battleBGM",
+  "if UniLib::AUDIO_FILE_REDIRECT[s]
+    s = music.is_a?(RPG::AudioFile) ? music.name : music;  music = Assets.get_asset(UniLib::AUDIO_FILE_REDIRECT, s)
+    return nil if $game_system.playing_bgm and Assets.strip_bgm(s) == Assets.strip_bgm($game_system.playing_bgm.name)
+  end")
