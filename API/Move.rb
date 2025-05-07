4172c96dd94466cf9678f78589fee6eb06dd56fa
birblett <bwee.sylv@gmail.com>
2025-05-07 02:50:34 -0700

    add basic move api

---
+++ API/Move.rb
@@ -0,0 +1,153 @@
+# ======================================================================================================================================== #
+# ============================================================= DEPENDENCIES ============================================================= #
+# ======================================================================================================================================== #
+
+UniLib.verify_version(0.6, __FILE__)
+
+# ======================================================================================================================================== #
+# ============================================================== PUBLIC API ============================================================== #
+# ======================================================================================================================================== #
+
+<<-DOC
+>> builder class for explicitly creating new moves
+DOC
+module MoveBuilder
+
+  <<-DOC
+  @param symbol - id to register under
+  @param name - move name, string
+  @param desc - move description, string
+  @param type - move type, symbol
+  @param category - move category, one of :physical, :special, :status
+  @param maxpp - move pp, int
+  @param basedamage - move damage, int - 0 will make the move non-damaging
+  @param accuracy - move accuracy, int - 0 is guaranteed hit
+  @param function - move function, int - for effects defined by overriding pbEffect, best formatted as hex - 0 has no special effects
+  @param target - move target, one of :User, :SingleNonUser, :AllNonUsers, :OppositeOpposing, :AllOpposing, :UserSide, :OpposingSide, :DragonDarts
+  @param priority - move priority, int
+  @param flags - additional move flags
+  >> creates a new move builder unless it already exists for the specified move; otherwise overwrites existing traits if specified
+  DOC
+  def self.add(symbol, name, desc, type, category, maxpp, basedamage = 0, accuracy = 0, function = 0, target = :SingleNonUser, priority = 0, flags = {})
+    m = MoveModifier.add(symbol).name(name).desc(desc).type(type).category(category).maxpp(maxpp).damage(basedamage).accuracy(accuracy)
+          .function(function).target(target).priority(priority)
+    flags.each { |f, v| m.flag(f, v) }
+    m
+  end
+
+end
+
+<<-DOC
+>> modifier class for modifying moves
+DOC
+class MoveModifier
+
+  <<-DOC
+  @param symbol - id to register under
+  >> registers a move modifier
+  DOC
+  def self.add(symbol)
+    CUSTOM_MOVES[symbol] = MoveModifier.new(symbol) if CUSTOM_MOVES[symbol].nil?
+    CUSTOM_MOVES[symbol]
+  end
+
+  <<-DOC
+  @param name - move name, string
+  >> sets the move name
+  DOC
+  def name(name)
+    @name = name
+    self
+  end
+
+  <<-DOC
+  @param desc - move description, string
+  >> sets the move description
+  DOC
+  def desc(desc)
+    @desc = desc
+    self
+  end
+
+  <<-DOC
+  @param type - move type, symbol
+  >> sets the move type
+  DOC
+  def type(type)
+    @type = type
+    self
+  end
+
+  <<-DOC
+  @param category - move category, one of :physical, :special, :status
+  >> sets the move category
+  DOC
+  def category(category)
+    @category = category if [:physical, :special, :status].include? category
+    self
+  end
+
+  <<-DOC
+  @param maxpp - move pp, int
+  >> sets the move max pp
+  DOC
+  def maxpp(max)
+    @maxpp = max
+    self
+  end
+
+  <<-DOC
+  @param basedamage - move damage, int - 0 will make the move non-damaging
+  >> sets the move base damage
+  DOC
+  def damage(damage)
+    @basedamage = damage
+    self
+  end
+
+  <<-DOC
+  @param accuracy - move accuracy, int - 0 is guaranteed hit
+  >> sets the move accuracy
+  DOC
+  def accuracy(accuracy)
+    @accuracy = accuracy
+    self
+  end
+
+  <<-DOC
+  @param target - move target, one of :User, :SingleNonUser, :AllNonUsers, :OppositeOpposing, :AllOpposing, :UserSide, :OpposingSide, :DragonDarts
+  >> sets the move targetting style
+  DOC
+  def target(target)
+    @target = target if [:User, :SingleNonUser, :AllNonUsers, :OppositeOpposing, :AllOpposing, :UserSide, :OpposingSide, :DragonDarts].include? target
+    self
+  end
+
+  <<-DOC
+  @param function - move function, int - for effects defined by overriding pbEffect, best formatted as hex - 0 has no special effects
+  >> sets the move function
+  DOC
+  def function(function)
+    @function = function
+    self
+  end
+
+  <<-DOC
+  @param priority - move priority, int
+  >> sets the move priority
+  DOC
+  def priority(priority)
+    @priority = priority
+    self
+  end
+
+  <<-DOC
+  @param flags - additional move flags
+  >> sets a move flag
+  DOC
+  def flag(flag, value)
+    @flags[flag] = value
+    self
+  end
+
+end
--- Core/MoveCore.rb
+++ Core/MoveCore.rb
@@ -10,9 +10,86 @@ UniLib.verify_version(0.6, __FILE__)
 
 module UniLib
 
+  CUSTOM_MOVES = {}
   MOVE_DATA = load_data("Data/moves.dat") unless defined? MOVE_DATA
+  MOVE_MAX_ID = MOVE_DATA.max_by { |_, v| v.flags[:ID].nil? ? 0 : v.flags[:ID] }[1].flags[:ID] unless defined? MOVE_MAX_ID
+  $move_current_max = MOVE_MAX_ID + 1
 
 end
+
+class MoveModifier
+
+  include UniLib
+
+  attr_accessor(:move)
+  attr_accessor(:name)
+  attr_accessor(:desc)
+  attr_accessor(:function)
+  attr_accessor(:type)
+  attr_accessor(:category)
+  attr_accessor(:basedamage)
+  attr_accessor(:accuracy)
+  attr_accessor(:maxpp)
+  attr_accessor(:target)
+  attr_accessor(:priority)
+  attr_accessor(:flags)
+
+  def initialize(symbol)
+    @symbol = symbol
+    @flags = {}
+    CUSTOM_MOVES[symbol] = self
+  end
+
+  def build
+    data = {}
+    m = MOVE_DATA[@symbol]
+    unless m.nil?
+      data[:name] = m.name
+      data[:desc] = m.desc
+      data[:function] = m.function
+      data[:type] = m.type
+      data[:category] = m.category
+      data[:basedamage] = m.basedamage
+      data[:accuracy] = m.accuracy
+      data[:maxpp] = m.maxpp
+      data[:target] = m.target
+      data[:priority] = m.priority
+      m.instance_variable_get(:@flags).each { |k, v| data[k] = v }
+    end
+    data[:name] = @name if @name
+    data[:desc] = @desc if @desc
+    data[:function] = @function if @function
+    data[:type] = @type if @type
+    data[:category] = @category if @category
+    data[:basedamage] = @basedamage if @basedamage
+    data[:accuracy] = @accuracy if @accuracy
+    data[:maxpp] = @maxpp if @maxpp
+    data[:target] = @target if @target
+    data[:priority] = @priority if @priority
+    @flags.each { |k, v| data[k] = v }
+    $cache.moves[@symbol] = MoveData.new(@symbol, data)
+    UniLib.dev_log(UniLib.obj_print($cache.moves[@symbol]))
+  end
+
+end unless UniLib.lib_loaded(__FILE__)
+
+# ======================================================================================================================================== #
+# ================================================================ EVENTS ================================================================ #
+# ======================================================================================================================================== #
+
+unless UniLib.lib_loaded(__FILE__)
+
+  def add_moves(save)
+    UniLib::CUSTOM_MOVES.each { |_, move_builder| move_builder.build }
+  end
+
+end
+
+UniLib.add_play_event(:add_moves, 1001)
+
+# ======================================================================================================================================== #
+# ================================================================ PATCH ================================================================= #
+# ======================================================================================================================================== #
 
 UniLib.replace_in_method(:PokeBattle_Move, :pbEffectMessages, "if !pbIsMultiHit && !attacker.effects[:ParentalBond]",
   "if !pbIsMultiHit and !attacker.effects[:ParentalBond] and !attacker.effects[:Multihit]")
