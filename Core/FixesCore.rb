f6cd0b3d523dd1969e90569d0f09556c5a0293f2
birblett <bwee.sylv@gmail.com>
2025-05-13 18:23:33 -0700

    fix instruct + struggle exception

--- Core/FixesCore.rb
+++ Core/FixesCore.rb
@@ -22,8 +22,8 @@ CrestBuilder.add_existing(:DEDECREST)
             .move_stat_override { |_, _, move| next :spe unless move.pbIsSpecial?(move) } if Rejuv
 
 # no longer replace their raw spa stat with def/atk stat with spe
-UniLib.replace_in_method(:PokeBattle_Battler, :crestStats, "@spatk = @defense", "true") if Rejuv
-UniLib.replace_in_method(:PokeBattle_Battler, :crestStats, "@attack = @speed", "true") if Rejuv
+UniLib.delete_in_method(:PokeBattle_Battler, :crestStats, "@spatk = @defense") if Rejuv
+UniLib.delete_in_method(:PokeBattle_Battler, :crestStats, "@attack = @speed") if Rejuv
 
 # move calculation to crest/itembuilder internals
 UniLib.replace_in_method(:PokeBattle_Move, :pbCalcDamage, "case attacker.crested", "case nil", 1)
@@ -33,7 +33,7 @@ CrestBuilder.add_existing(Reborn ? :CHERRIMCREST : :CHERCREST)
             .add_receiver(:CHERRIM, "Sunshine")
             .damage_mod { |_, _, move, _, is_ai| next 1.5 if !is_ai and move.pbIsPhysical?(move) }
 
-# fix silent crash when sleep talk called with less than 4 moves in moveset
+# fix silent exception when sleep talk called with less than 4 moves in moveset
 UniLib.replace_in_method(:PokeBattle_Move_0B4, :pbEffect, "choices = (0...4).to_a.select{|i| (attacker.moves[i].move.is_a?(Symbol)) && !blacklist.include?(attacker.moves[i].move) && @battle.pbCanChooseMove?(attacker.index,i,false,{sleeptalk: true})}",
   "choices = (0...attacker.moves.length).to_a.select{|i| (attacker.moves[i].move.is_a?(Symbol)) && !blacklist.include?(attacker.moves[i].move) && @battle.pbCanChooseMove?(attacker.index,i,false,{sleeptalk: true})}") if Rejuv
 UniLib.replace_in_method(:PokeBattle_AI, :sleeptalkcode, "for i in 0..3", "for i in 0...@attacker.moves.length")
@@ -50,3 +50,6 @@ UniLib.insert_in_method(:PokeBattle_AI, :pbTypeModNoMessages, :HEAD,
 
 # fixes primal reversion
 UniLib.insert_in_method(:PokeBattle_Battler, :pbAbilitiesOnSwitchIn, "@pokemon.makePrimal", "self.form = @pokemon.form")
+
+# fix instruct + struggle exception
+UniLib.insert_in_method_before(:PokeBattle_AI, :instructcode, "lastmove = @attacker.pbPartner.lastMoveUsed", "return 0 if lastmove  == :STRUGGLE")
